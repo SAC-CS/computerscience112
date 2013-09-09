1929ebf42204f6c977853cba2816817c9ee05d18
juanhernandez112 <juanphdez@gmail.com>
2013-09-09 00:01:27 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+// Hello World
+// Juan P Hernandez
+// This program outputs a message
+import java.util.Scanner;
+
+public class HelloWorld
+{
+ 
+    public static void main(String[] args)
+    {
+        
+        Scanner input = new Scanner( System.in );
+        //Variables
+        String name, message, hello;
+        System.out.println("What's your name?");
+        name =input.nextLine();
+        hello = "Hello ";
+        message = hello + name;
+        System.out.println(message);
+      
+    }
+
+}
